653e45b8ebf8c8788e696dec738b7e8eefd2c96f
Joshua Harris <joshuadaleharris@gmail.com>
2018-08-18 00:22:11 -0400

    Removing app startup message

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-//import PRHandler from './lib/pr-handler';
+const Handler = require('./lib/handler');
 
 /**
  * This is the entry point for your Probot App.
  * @param {import('probot').Application} app - Probot's Application class.
  */
-module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
-
-  app.on(['pull_request.opened', 'pull_request.edited'], async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
-  })
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-}
+module.exports = function (app) {
+  app.on([
+    'pull_request.opened',
+    'pull_request.reopened',
+    'pull_request.synchronize',
+    'pull_request.edited'
+  ], async context => {
+    new Handler(context).execute();
+  });
+};
